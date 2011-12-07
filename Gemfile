83961b6629614d9d71768a384e6b74cc78521303
Aaron Lerch <aaronlerch@gmail.com>
2011-12-07 10:28:19 -0500

    Add redis gem, add sinatra configuration to initialize REDIS

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'slim'
+gem 'redis'
--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 require 'slim'
+require 'redis'
+
+configure do
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 get '/' do
   slim :index
