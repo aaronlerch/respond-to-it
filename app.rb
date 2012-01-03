ac2c6c2ca53b7619cf6e38e2fbbf8568d764ccec
Aaron Lerch <aaronlerch@gmail.com>
2012-01-02 23:43:49 -0500

    Begin to package up requests to store and retrieve

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require 'redis'
 require 'json'
 require 'sinatra/flash'
 
+class String
+  def titlecase
+    tr('_', ' ').
+    gsub(/\s+/, ' ').
+    gsub(/\b\w/){ $`[-1,1] == "'" ? $& : $&.upcase }
+  end
+end
+
 configure :development do
   uri = URI.parse('redis://localhost:6379')
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
@@ -19,11 +27,11 @@ configure do
 end
 
 helpers do
-  def config_key(code)
+  def config_key
     "code:#{params[:code]}:config"
   end
 
-  def requests_key(code)
+  def requests_key
     "code:#{params[:code]}:requests"
   end
 
@@ -51,9 +59,47 @@ helpers do
 
   def config
     @config ||= begin
-      data = REDIS.get config_key(params[:code])
+      data = REDIS.get config_key
       result = JSON.parse(data) unless data.nil?
     end
+  end
+
+  def requests
+    @requests ||= begin
+      data = REDIS.lrange requests_key, 0, 9
+      # todo: parse the json and return
+      puts data.inspect
+    end
+  end
+
+  def store_request
+    REDIS.multi do
+      REDIS.lpush requests_key, package_request
+      REDIS.ltrim requests_key, 0, 9 # restrict to 10 items
+    end
+  end
+
+  def package_request
+    {
+      :time => Time.now.utc.to_i,
+      :ip => request.ip,
+      :method => request.request_method.upcase,
+      :path => request.fullpath,
+      :headers => package_headers
+      # todo: store the body and form post data as well
+    }.to_json
+  end
+
+  def package_headers
+    pretty = {}
+    allowed_headers = request.env.reject { |k,v| k =~ /^HTTP_.*/ }
+    allowed_headers.each do |k,v|
+      header = k.dup
+      header.gsub!(/HTTP_/, '')
+      header = header.downcase.titlecase.tr(' ', '-')
+      pretty[header] = v
+    end
+    return pretty
   end
 end
 
@@ -61,10 +107,17 @@ get '/' do
   slim :index
 end
 
+get '/test' do
+  result = ""
+  result
+end
+
 ['/:code.:format?', '/:code'].each do |path|
   get path do
     return slim(:view) if view?
     return [404, "Um, guess again?"] if config.nil?
+
+    store_request
 
     if json?
       json
@@ -78,12 +131,14 @@ end
   post path do
     if view?
       config_hash = {"json" => params["json"].to_s, "xml" => params["xml"].to_s}
-      REDIS.set config_key(params[:code]), config_hash.to_json
+      REDIS.set config_key, config_hash.to_json
       flash[:notice] = "The response was updated successfully."
       return slim(:view)
     end
 
     return 404 if config.nil?
+
+    store_request
 
     if json?
       json
